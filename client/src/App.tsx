c2bf856a75a421b4c8726cc78cb319af39aa204a
LoyalEnv0y <cetin200012@gmail.com>
2023-09-23 12:32:48 +0300

    Abstracted the section element to a component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,25 @@ import SectionTitle from './components/SectionTitle';
 import Header from './components/Header';
 import PictureBox from './components/PictureBox';
 import About from './components/About';
+import SectionContainer from './components/SectionContainer';
 
 const App = () => {
 	return (
 		<div className="overflow-x-hidden">
 			<Header />
 
-			<main className="my-10 flex min-h-screen w-screen flex-col items-center px-5">
-				<section className="flex w-full flex-col items-center justify-center gap-y-5 font-rubik">
-					<SectionTitle text="Hakkımda" />
+			<main className="my-10 flex min-h-screen w-screen flex-col items-center gap-y-10 px-5 font-rubik">
+				<SectionContainer className="gap-y-5">
+					<SectionTitle text="HAKKIMDA" />
 
 					<PictureBox />
 
 					<About />
-				</section>
+				</SectionContainer>
+
+				<SectionContainer>
+					<SectionTitle text="TEKNOLOJİLER" />
+				</SectionContainer>
 			</main>
 		</div>
 	);
